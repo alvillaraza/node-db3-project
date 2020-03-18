2b7f58dc559115d0d5fa2b727b5f7946d5a505d4
alexis villaraza <alexis.borja@gmail.com>
2020-03-18 16:11:35 -0700

    answered all queries

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,49 +3,55 @@ const db = require("../data/db-config.js");
 module.exports = {
   find,
   findById,
-  // findSteps,
+  findSteps,
   add,
-  // addStep,
-  update, 
+  addStep,
+  update,
   remove
 };
 
 function find() {
-  return db('schemes');
-};
-
-function findById(id) {
-  return db('schemes')
-    .where({ id })
-    .first();
-};
-
-// function findSteps() {
-//   return db('schemes')
-//   .where({steps})
-// }
-
-function add(schemeData) {
-  return db('schemes')
-  .insert(schemeData)
-};
-
-// function addStep(id, stepData) {
-//   return db('steps')
-//     .where({id})
-//   .insert(stepData)
-// }
-
-function update(changes, id) {
-  return db('schemes')
-    .where({ id })
-    .update(changes)
-};
-
-function remove(id) {
-  return db('schemes')
-  .where("id", id)
-    .del();
-  
+  return db("schemes");
 }
 
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first();
+}
+
+function findSteps(schemesid) {
+  return db("steps")
+    .join("steps.scheme_id", "schemes.id")
+    .select(
+      "steps.id",
+      "steps.instructions",
+      "schemes.id as schemesid",
+      "schemes.scheme_name"
+    )
+    .where({ scheme_id: schemes.id });
+}
+
+function add(schemeData) {
+  return db("schemes").insert(schemeData);
+}
+
+function addStep(stepData, id) {
+  return (
+    db("steps")
+      // .where({id})
+      .insert(stepData, id)
+  );
+}
+
+function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db("schemes")
+    .where("id", id)
+    .del();
+}
