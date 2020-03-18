bbf93da3c617ba1a34c9ea4953d23e7f77c8b8d8
alexis villaraza <alexis.borja@gmail.com>
2020-03-18 14:12:29 -0700

    coded find in scheme-model

--- queries.sql
+++ queries.sql
@@ -1,8 +1,13 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+SELECT c.CategoryName as Category, p.* 
+FROM Product as p
+LEFT JOIN Category as c ON p.Id = c.Id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+SELECT Id, ShipName from [Order]
+WHERE OrderDate < "2012-08-09";
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
